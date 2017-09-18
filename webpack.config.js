c1198c5d486c8f532426dc4bfd419fb01cc4212a
asr <ajsoriar@gmail.com>
2017-09-18 09:51:47 +0200

    add script to start dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 /*
     ./webpack.config.js
 */
+
+/*
 const path = require('path');
 module.exports = {
   entry: './src/index.js',
@@ -15,3 +17,62 @@ module.exports = {
     ]
   }
 }
+*/
+
+
+
+
+
+/* 
+    ./webpack.config.js
+*/
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+
+  plugins: [HtmlWebpackPluginConfig]
+}
+
+
+
+
+
+/*
+
+
+
+module.exports = {
+
+...
+
+module: {
+    loaders: [
+        ...
+    ]
+},
+// add this line
+plugins: [HtmlWebpackPluginConfig]
+}
+
+*/
