955acb38d2bc99ddf40c73c61a0b5de4c28e1e8d
asr <ajsoriar@gmail.com>
2018-08-21 16:26:15 +0200

    Improve funcionality

--- src/react-string-avatar.js
+++ src/react-string-avatar.js
@@ -2,51 +2,68 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class StringAvatar extends React.Component {
-
-  //static displayName = 'StringAvatar';
+  // static displayName = 'StringAvatar';
 
   _renderImage() {
-
     var _long = 45,
-      //_picture_resolution = 256,
-      //_wrapper = true,
-      _str = "", //scope.initials || "",
-      _bgColor = "#000",
-      //_textColor = "#fff",
+      // _picture_resolution = 256,
+      // _wrapper = true,
+      _str = '', // scope.initials || "",
+      _bgColor = '#000',
+      // _textColor = "#fff",
       _pixelated = false,
       _img_styling = { verticalAlign: 'top' },
       _roundShape = false,
-      _defaultWrapperStyling = "border-radius:0; display:block; overflow:hidden;",
-      //_extra_classes = "",
-      //_extra_img_classes = "",
-      //_extra_styles = "",
+      _defaultWrapperStyling =
+        'border-radius:0; display:block; overflow:hidden;',
+      // _extra_classes = "",
+      // _extra_img_classes = "",
+      // _extra_styles = "",
       _corner_radius = 0,
-      //_picture_format = "png",
-      _colors_palette = ["#bdc3c7", "#6f7b87", "#2c3e50", "#2f3193", "#662d91", "#922790", "#ec2176", "#ed1c24", "#f36622", "#f8941e", "#fab70f", "#fdde00", "#d1d219", "#8ec73f", "#00a650", "#00aa9c", "#00adef", "#0081cd", "#005bab"],
+      // _picture_format = "png",
+      _colors_palette = [
+        '#bdc3c7',
+        '#6f7b87',
+        '#2c3e50',
+        '#2f3193',
+        '#662d91',
+        '#922790',
+        '#ec2176',
+        '#ed1c24',
+        '#f36622',
+        '#f8941e',
+        '#fab70f',
+        '#fdde00',
+        '#d1d219',
+        '#8ec73f',
+        '#00a650',
+        '#00aa9c',
+        '#00adef',
+        '#0081cd',
+        '#005bab'
+      ],
       _autoColor = false;
-      //_font_weight = 100,
-      //_font_scale = 100,
-      //_text_shadow = false,
-      //_bind = false,
-      //_img_width = "100%",
-      //_upperCase = false;
-
+    // _font_weight = 100,
+    // _font_scale = 100,
+    // _text_shadow = false,
+    // _bind = false,
+    // _img_width = "100%",
+    // _upperCase = false;
 
     // -----------------------------
     // utility functions
     // -----------------------------
 
     function generateAvatar(text, w, h, bgColor, bgImage, props) {
-
-      var WIDTH = 256,
+      let WIDTH = 256,
         HEIGHT = 256,
         canvas,
         ctx,
         font_size;
 
-      //console.log("w:", w);
-      //console.log("h:", h);
-      //console.log("text:", text);
+      // console.log("w:", w);
+      // console.log("h:", h);
+      // console.log("text:", text);
 
       if (w != undefined && w > 0) {
         if (h != undefined && h > 0) {
@@ -56,7 +73,7 @@ class StringAvatar extends React.Component {
       }
 
       canvas = document.createElement('canvas');
-      canvas.id = "ngAvatar-" + Date.now();
+      canvas.id = `ngAvatar-${  Date.now()}`;
       canvas.width = WIDTH;
       canvas.height = HEIGHT;
 
@@ -66,37 +83,36 @@ class StringAvatar extends React.Component {
 
       font_size = WIDTH / (2 / (props.fontScale / 100));
 
-      //console.log("font_size:", font_size);
+      // console.log("font_size:", font_size);
 
-      ctx.font = props.fontWeight + " " + font_size + "px sans-serif";
+      ctx.font = `${props.fontWeight} ${font_size}px sans-serif`;
 
       if (props.textShadow === true) {
-        ctx.shadowColor = "black";
+        ctx.shadowColor = 'black';
         ctx.shadowOffsetX = 0;
         ctx.shadowOffsetY = 0;
         ctx.shadowBlur = 5;
       }
 
-      ctx.textAlign = "center";
+      ctx.textAlign = 'center';
       ctx.fillStyle = props.textColor;
-      //ctx.fillText(_text, WIDTH / 2, HEIGHT - (HEIGHT / 2) + (_default.font_size / 3) + 5 );
-      ctx.fillText(text, WIDTH / 2, HEIGHT - (HEIGHT / 2) + (font_size / 3));
+      // ctx.fillText(_text, WIDTH / 2, HEIGHT - (HEIGHT / 2) + (_default.font_size / 3) + 5 );
+      ctx.fillText(text, WIDTH / 2, HEIGHT - HEIGHT / 2 + font_size / 3);
 
-      return canvas.toDataURL("image/" + props.pictureFormat);
+      return canvas.toDataURL(`image/${  props.pictureFormat}`);
     }
 
     function getInitialsFromString(str) {
+      // console.log("str:", str);
 
-      //console.log("str:", str);
-
-      if (!str) return ""
+      if (!str) return '';
 
       var output = '',
         i = 0,
         str = str.split(' '),
         len = str.length;
 
-      for (i; i < len; i++) if (str[i] != "") output += str[i][0]; //.toUpperCase();
+      for (i; i < len; i++) if (str[i] != '') output += str[i][0]; // .toUpperCase();
 
       return output;
     }
@@ -105,7 +121,7 @@ class StringAvatar extends React.Component {
     // checkValues
     // -----------------------------
 
-    //console.log("this.props:", this.props);
+    // console.log("this.props:", this.props);
 
     // Create text to be shown
 
@@ -117,22 +133,20 @@ class StringAvatar extends React.Component {
       _str = getInitialsFromString(this.props.string);
     }
 
-
     // Calculate color
 
     if (this.props.bgColor != undefined) {
       _bgColor = this.props.bgColor;
-    } else {
-
-      if (this.props.autoColor != undefined) {
-
-        _autoColor = this.props.autoColor;
-        if (_autoColor === true) {
-          var i, lon = _str.length, charIndex = 0, colorIndex;
-          for (i = 0; i < lon; i++) charIndex = _str.charCodeAt(i);
-          colorIndex = charIndex % _colors_palette.length;
-          _bgColor = _colors_palette[colorIndex];
-        }
+    } else if (this.props.autoColor != undefined) {
+      _autoColor = this.props.autoColor;
+      if (_autoColor === true) {
+        var i,
+          lon = _str.length,
+          charIndex = 0,
+          colorIndex;
+        for (i = 0; i < lon; i++) charIndex = _str.charCodeAt(i);
+        colorIndex = charIndex % _colors_palette.length;
+        _bgColor = _colors_palette[colorIndex];
       }
     }
 
@@ -140,75 +154,92 @@ class StringAvatar extends React.Component {
     // Create the image here
     // --------------------------
 
-    var imgData = generateAvatar(
+    const imgData = generateAvatar(
       _str,
       this.props.pictureResolution,
       this.props.pictureResolution,
-      _bgColor, //this.props.bgColor,
+      _bgColor, // this.props.bgColor,
       null,
       this.props
     );
-
 
     // ------------------------------------------
     // Create HTML and styles wraping the image
     // ------------------------------------------
 
-    var _img_styling = { imageRendering: 'pixelated', imageRendering: '-moz-crisp-edges' };
+    var _img_styling = {
+      imageRendering: 'pixelated',
+      imageRendering: '-moz-crisp-edges'
+    };
 
     var _pixelated = this.props.pixelated;
     if (_pixelated === true) {
-      _img_styling.imageRendering = "pixelated";
-      //_img_styling.imageRendering = "-moz-crisp-edges";
+      _img_styling.imageRendering = 'pixelated';
+      // _img_styling.imageRendering = "-moz-crisp-edges";
     }
 
-    var _defaultWrapperStyling = { overflow: 'hidden', width: this.props.width +"px", height: this.props.width +"px", boxSizing: "unset" };
+    var _defaultWrapperStyling = {
+      overflow: 'hidden',
+      width: `${this.props.width  }px`,
+      height: `${this.props.width  }px`,
+      boxSizing: 'unset'
+    };
 
     if (this.props.roundShape != undefined) {
-      //console.log("1");
+      // console.log("1");
       _roundShape = this.props.roundShape;
       if (_roundShape) {
-        //console.log("2");
-        _defaultWrapperStyling.borderRadius = this.props.width +"px";
+        // console.log("2");
+        _defaultWrapperStyling.borderRadius = `${this.props.width}px`;
       }
     } else {
-      //console.log("3");
+      // console.log("3");
       if (this.props.cornerRadius != undefined) {
-        //console.log("4");
+        // console.log("4");
         _corner_radius = this.props.cornerRadius;
-        _defaultWrapperStyling.borderRadius = _corner_radius +"px";
+        _defaultWrapperStyling.borderRadius = `${_corner_radius}px`;
       }
     }
-    
-    //console.log(">> this.props.wrapperStyle:", this.props.wrapperStyle );
-    //console.log(">> _defaultWrapperStyling:", _defaultWrapperStyling );
 
-    _defaultWrapperStyling = Object.assign({}, this.props.wrapperStyle, _defaultWrapperStyling );
-    //console.log(">> _defaultWrapperStyling B:", _defaultWrapperStyling );
-    
-    
-    //var imgHtml = '<img src={imgData} style={_img_styling} width={this.props.width} height="" />';
-    var imgHtml = <img src={imgData} style={_img_styling} width={this.props.width} height="" />;
+    // console.log(">> this.props.wrapperStyle:", this.props.wrapperStyle );
+    // console.log(">> _defaultWrapperStyling:", _defaultWrapperStyling );
+
+    _defaultWrapperStyling = Object.assign(
+      {},
+      this.props.wrapperStyle,
+      _defaultWrapperStyling
+    );
+    // console.log(">> _defaultWrapperStyling B:", _defaultWrapperStyling );
+
+    // var imgHtml = '<img src={imgData} style={_img_styling} width={this.props.width} height="" />';
+    const imgHtml = (
+      <img
+        src={imgData}
+        style={_img_styling}
+        width={this.props.width}
+        height=""
+      />
+    );
 
     if (this.props.wrapper) {
-      //return '<div className="avatar-wrapper " style={_defaultWrapperStyling} >{imgHtml}</div>';
-      return <div className="avatar-wrapper " style={_defaultWrapperStyling} >{imgHtml}</div>;
-    } else {
-      return imgHtml
+      // return '<div className="avatar-wrapper " style={_defaultWrapperStyling} >{imgHtml}</div>';
+      return (
+        <div className="avatar-wrapper " style={_defaultWrapperStyling}>
+          {imgHtml}
+        </div>
+      );
     }
-
-  } //this.props.renderImage ends here.
+    return imgHtml;
+  } // this.props.renderImage ends here.
 
   render() {
     return this._renderImage();
   }
-
 }
-
 
 StringAvatar.propTypes = {
   initials: PropTypes.string,
-  //width: PropTypes.string,
+  // width: PropTypes.string,
   wrapper: PropTypes.bool,
   bgColor: PropTypes.string,
   textColor: PropTypes.string,
@@ -218,10 +249,10 @@ StringAvatar.propTypes = {
   roundShape: PropTypes.bool,
   defaultWrapperStyling: PropTypes.object,
   wrapperStyle: PropTypes.object,
-  //class: PropTypes.string, //*
-  //imgClass: PropTypes.string, //*
-  
-  string: PropTypes.string, //*
+  // class: PropTypes.string, //*
+  // imgClass: PropTypes.string, //*
+
+  string: PropTypes.string, //* 
   cornerRadius: PropTypes.number,
   pictureFormat: PropTypes.string,
   colorsPalette: PropTypes.array,
@@ -230,38 +261,61 @@ StringAvatar.propTypes = {
   fontScale: PropTypes.number,
   textShadow: PropTypes.bool,
   bind: PropTypes.bool,
-  //maxLength: PropTypes.string, //*
-  upperCase: PropTypes.bool,
+  // maxLength: PropTypes.string, //*
+  upperCase: PropTypes.bool
 };
-
 
 StringAvatar.defaultProps = {
   initials: null,
   wrapper: true,
-  bgColor: "#000",
-  textColor: "#fff",
-  pictureResolution: 50, //256,
-  width: 60,
+  bgColor: '#000',
+  textColor: '#fff',
+  pictureResolution: 256, // 256,
+  width: 45,
   pixelated: false,
-  //imgStyling: "vertical-align:top;",
+  // imgStyling: "vertical-align:top;",
   roundShape: false,
-  defaultWrapperStyling: { 'borderRadius': 0, 'display': 'block', 'overflow': 'hidden' },
+  defaultWrapperStyling: {
+    borderRadius: 0,
+    display: 'block',
+    overflow: 'hidden'
+  },
   wrapperStyle: {},
-  //extraClasses: "",
-  //extraImgClasses: "",
-  //extraStyles: "",
-  
+  // extraClasses: "",
+  // extraImgClasses: "",
+  // extraStyles: "",
+
   string: null,
   cornerRadius: 0,
-  pictureFormat: "png",
-  colorsPalette: ["#bdc3c7", "#6f7b87", "#2c3e50", "#2f3193", "#662d91", "#922790", "#ec2176", "#ed1c24", "#f36622", "#f8941e", "#fab70f", "#fdde00", "#d1d219", "#8ec73f", "#00a650", "#00aa9c", "#00adef", "#0081cd", "#005bab"],
+  pictureFormat: 'png',
+  colorsPalette: [
+    '#bdc3c7',
+    '#6f7b87',
+    '#2c3e50',
+    '#2f3193',
+    '#662d91',
+    '#922790',
+    '#ec2176',
+    '#ed1c24',
+    '#f36622',
+    '#f8941e',
+    '#fab70f',
+    '#fdde00',
+    '#d1d219',
+    '#8ec73f',
+    '#00a650',
+    '#00aa9c',
+    '#00adef',
+    '#0081cd',
+    '#005bab'
+  ],
   autoColor: false,
   fontWeight: 100,
   fontScale: 100,
   textShadow: false,
   bind: false,
-  //imgWidth: "100%",
-  upperCase: false,
+  // imgWidth: "100%",
+  upperCase: false
 };
 
 export default StringAvatar;
