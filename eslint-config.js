170c7fe954874b3ffdf842e9c50f4dd0917dd72b
asr <ajsoriar@gmail.com>
2019-12-24 16:10:48 +0100

    Add linter

---
+++ eslint-config.js
@@ -0,0 +1,138 @@
+// 2019-12-22
+// $ npm init
+// $ npm i -D eslint prettier eslint-plugin-prettier eslint-config-prettier
+// $ npm install --save-dev eslint-config-airbnb eslint eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react eslint-plugin-react-hooks
+
+module.exports = {
+    // Extending Configuration Files
+
+    // Example 1:
+
+    // {
+    //     "extends": "./node_modules/coding-standard/.eslintrc",
+    //     "rules": {
+    //         // Override any settings from the "parent" configuration
+    //         "lol": 1
+    //     }
+    // }
+
+    // Example 2:
+
+    // Remove map(require.resolve)
+
+    // As a note, require.resolve allows you to look up and return the location of a module without loading it. (https://bramanti.me/organization-eslint-config/) 2016
+
+    // extends: [ // Configurations may also be provided as an array, with additional files overriding any properties provided in the previous file.
+    //     ...
+    // ].map(require.resolve), // The map(require.resolve) pattern was needed for linking but we don't use that solution anymore. (https://github.com/schibsted/eslint-config-schibsted/issues/62) (https://github.com/schibsted/eslint-config-schibsted/pull/45) 2017
+
+    extends: [
+        // Configurations may also be provided as an array, with additional files overriding any properties provided in the previous file.
+        'airbnb',
+        'prettier',
+        'prettier/react' // The prefix eslint-config- looks like is not needed.
+    ],
+
+    globals: {
+        // https://eslint.org/docs/1.0.0/user-guide/configuring
+        window: true,
+        document: true
+    },
+
+    plugins: [ // ESLint supports the use of third-party plugins. Before using the plugin you have to install it using npm.
+        'react', // The eslint-plugin- prefix can be omitted from the plugin name.
+        'prettier'
+    ],
+    parserOptions: {
+        // By default, ESLint expects ECMAScript 5 syntax.
+        // You can override that setting to enable support for other ECMAScript versions as well as JSX by using parser options.
+        // More info here: https://eslint.org/docs/user-guide/configuring
+
+        ecmaVersion: 8,
+        sourceType: 'module',
+        ecmaFeatures: {
+            // experimentalObjectRestSpread: true,
+            jsx: true // Note that supporting JSX syntax is not the same as supporting React. React applies specific semantics to JSX syntax that ESLint doesn't recognize. We recommend using eslint-plugin-react if you are using React
+        }
+    },
+    rules: {
+        indent: 0,
+        curly: ['error', 'all'], // Force the use of { and } everywhere
+        'import/prefer-default-export': 0, // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
+        'no-alert': 'error',
+        'no-debugger': 'error',
+        'no-console': [
+            'warn',
+            {
+                allow: ['warn', 'error']
+            }
+        ],
+        'linebreak-style': 0, // https://eslint.org/docs/rules/linebreak-style
+
+        // * Comments *
+
+        'lines-around-comment': ['error', {
+            beforeBlockComment: true
+        }],
+        'multiline-comment-style': ['error', 'separate-lines'],
+        'capitalized-comments': [
+            'error',
+            'always',
+            {
+                ignoreConsecutiveComments: true
+            }
+        ],
+
+        // * eslint-plugin-react *
+
+        // https://github.com/yannickcr/eslint-plugin-react
+        // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
+
+        'react/jsx-filename-extension': [
+            1,
+            {
+                // If you wanted to allow both '.jsx' and '.js', the configuration would be this.
+                extensions: ['.js', '.jsx']
+            }
+        ],
+
+        // * PRETTIER *
+
+        // https://prettier.io/docs/en/options.html#trailing-commas
+
+        'prettier/prettier': [
+            'error',
+            {
+                tabWidth: 4,
+
+                singleQuote: true, // JSX quotes ignore this option – see jsx-single-quote.
+
+                // - Semicolons -
+                semi: true,
+
+                // true - Add a semicolon at the end of every statement.
+                // false - Only add semicolons at the beginning of lines that may introduce ASI failures.
+
+                // https://prettier.io/docs/en/rationale.html#semicolons
+                // https://standardjs.com/rules.html#semicolons
+
+
+                // - Trailing Commas -
+
+                // JavaScript has allowed trailing commas in array literals since the beginning, and later added them to object literals (ECMAScript 5) and most recently (ECMAScript 2017) to function parameters.
+                // JSON, however, disallows trailing commas.
+                trailingComma: 'none',
+
+                // "none" - No trailing commas.
+                // "es5" - Trailing commas where valid in ES5 (objects, arrays, etc.)
+                // "all" - Trailing commas wherever possible (including function arguments). This requires node 8 or a transform.
+
+                jsxBracketSameLine: false
+            }
+        ]
+    },
+    env: {
+        browser: true,
+        node: true
+    }
+};
--- src/react-string-avatar.js
+++ src/react-string-avatar.js
@@ -1,205 +1,214 @@
-import React from 'react';
+import React, { Component} from 'react';
 
-class StringAvatar extends React.Component {
-  generateAvatar(text, w, h, bgColor, bgImage, props) {
-    let WIDTH = 256,
-      HEIGHT = 256,
-      canvas,
-      ctx,
-      font_size;
+class StringAvatar extends Component {
+    
+    generateAvatar (text, w, h, bgColor, bgImage, props) {
+        let WIDTH = 256;
+        let HEIGHT = 256;
+        let canvas;
+        let ctx;
+        let fontsize;
 
-    if (w !== undefined && w > 0) {
-      if (h !== undefined && h > 0) {
-        WIDTH = w;
-        HEIGHT = h;
-      }
-    }
+        if (w !== undefined && w > 0) {
+            if (h !== undefined && h > 0) {
+                WIDTH = w;
+                HEIGHT = h;
+            }
+        }
 
-    canvas = document.createElement('canvas');
-    canvas.id = `ngAvatar-${Date.now()}`;
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
+        canvas = document.createElement('canvas');
+        canvas.id = `ngAvatar-${Date.now()}`;
+        canvas.width = WIDTH;
+        canvas.height = HEIGHT;
 
-    ctx = canvas.getContext('2d');
-    ctx.fillStyle = bgColor;
-    ctx.fillRect(0, 0, WIDTH, HEIGHT);
-    font_size = WIDTH / (2 / (props.fontScale / 100));
-    ctx.font = `${props.fontWeight} ${font_size}px sans-serif`;
+        ctx = canvas.getContext('2d');
+        ctx.fillStyle = bgColor;
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        fontsize = WIDTH / (2 / (props.fontScale / 100));
+        ctx.font = `${props.fontWeight} ${fontsize}px sans-serif`;
 
-    if (props.textShadow === true) {
-      ctx.shadowColor = 'black';
-      ctx.shadowOffsetX = 0;
-      ctx.shadowOffsetY = 0;
-      ctx.shadowBlur = 5;
-    }
+        if (props.textShadow === true) {
+            ctx.shadowColor = 'black';
+            ctx.shadowOffsetX = 0;
+            ctx.shadowOffsetY = 0;
+            ctx.shadowBlur = 5;
+        }
 
-    ctx.textAlign = 'center';
-    ctx.fillStyle = props.textColor;
-    // ctx.fillText(_text, WIDTH / 2, HEIGHT - (HEIGHT / 2) + (_default.font_size / 3) + 5 );
-    ctx.fillText(text, WIDTH / 2, HEIGHT - HEIGHT / 2 + font_size / 3);
+        ctx.textAlign = 'center';
+        ctx.fillStyle = props.textColor;
+        // Ctx.fillText(text, WIDTH / 2, HEIGHT - (HEIGHT / 2) + (default.fontsize / 3) + 5 );
+        ctx.fillText(text, WIDTH / 2, HEIGHT - HEIGHT / 2 + fontsize / 3);
 
-    return canvas.toDataURL(`image/${props.pictureFormat}`);
-  }
-
-  getInitialsFromString(str) {
-    if (!str) return '';
-    str = str.split(' ');
-    let output = '',
-      i = 0,
-      len = str.length;
-    for (i; i < len; i += 1) if (str[i] !== '') output += str[i][0]; // .toUpperCase();
-    return output;
-  }
-  _renderImage() {
-    // let // _long = 45,
-    // _picture_resolution = 256,
-    // _wrapper = true,
-    let _str = '',
-      _bgColor = '#000',
-      // _textColor = "#fff",
-      _pixelated = false,
-      _img_styling = { verticalAlign: 'top' },
-      _roundShape = false,
-      _defaultWrapperStyling =
-        'border-radius:0; display:block; overflow:hidden;',
-      // _extra_classes = "",
-      // _extra_img_classes = "",
-      // _extra_styles = "",
-      _corner_radius = 0,
-      // _picture_format = "png",
-      _colors_palette = [
-        '#bdc3c7',
-        '#6f7b87',
-        '#2c3e50',
-        '#2f3193',
-        '#662d91',
-        '#922790',
-        '#ec2176',
-        '#ed1c24',
-        '#f36622',
-        '#f8941e',
-        '#fab70f',
-        '#fdde00',
-        '#d1d219',
-        '#8ec73f',
-        '#00a650',
-        '#00aa9c',
-        '#00adef',
-        '#0081cd',
-        '#005bab'
-      ],
-      _autoColor = false;
-
-    // Create text to be shown
-
-    if (this.props.initials != undefined) {
-      _str = this.props.initials;
-    }
-
-    if (this.props.string != undefined) {
-      _str = this.getInitialsFromString(this.props.string);
-    }
-
-    // Calculate color
-
-    if (this.props.bgColor != undefined) {
-      _bgColor = this.props.bgColor;
-    } else if (this.props.autoColor != undefined) {
-      _autoColor = this.props.autoColor;
-      if (_autoColor === true) {
-        let i,
-          lon = _str.length,
-          charIndex = 0,
-          colorIndex;
-        for (i = 0; i < lon; i += 1) charIndex = _str.charCodeAt(i);
-        colorIndex = charIndex % _colors_palette.length;
-        _bgColor = _colors_palette[colorIndex];
-      }
-    }
-
-    // --------------------------
-    // Create the image here
-    // --------------------------
-
-    const imgData = this.generateAvatar(
-      _str,
-      this.props.pictureResolution,
-      this.props.pictureResolution,
-      _bgColor, // this.props.bgColor,
-      null,
-      this.props
-    );
-
-    // ------------------------------------------
-    // Create HTML and styles wraping the image
-    // ------------------------------------------
-
-    _img_styling = {
-      imageRendering: 'pixelated',
-      imageRendering: '-moz-crisp-edges'
+        return canvas.toDataURL(`image/${props.pictureFormat}`);
     };
 
-    _pixelated = this.props.pixelated;
-    if (_pixelated === true) {
-      _img_styling.imageRendering = 'pixelated';
-      // _img_styling.imageRendering = "-moz-crisp-edges";
+    getInitialsFromString(str) {
+        if (!str) {return '';}
+        str = str.split(' ');
+        let output = '';
+            let i = 0;
+            let len = str.length;
+        for (i; i < len; i += 1) {if (str[i] !== '') output += str[i][0];} // .toUpperCase();
+        return output;
     }
 
-    _defaultWrapperStyling = {
-      overflow: 'hidden',
-      width: `${this.props.width}px`,
-      height: `${this.props.width}px`,
-      boxSizing: 'unset'
-    };
+    renderImage() {
+        // Let // long = 45,
+        // pictureresolution = 256,
+        // wrapper = true,
+        let str = '';
+            let bgColor = '#000';
+            // textColor = "#fff",
+            let pixelated = false;
+            let imgstyling = {
+                verticalAlign: 'top'
+            };
+            let roundShape = false;
+            let defaultWrapperStyling =
+            'border-radius:0; display:block; overflow:hidden;';
+            // extraclasses = "",
+            // extraimgclasses = "",
+            // extrastyles = "",
+            let cornerradius = 0;
+            // pictureformat = "png",
+            let colorspalette = [
+                '#bdc3c7',
+                '#6f7b87',
+                '#2c3e50',
+                '#2f3193',
+                '#662d91',
+                '#922790',
+                '#ec2176',
+                '#ed1c24',
+                '#f36622',
+                '#f8941e',
+                '#fab70f',
+                '#fdde00',
+                '#d1d219',
+                '#8ec73f',
+                '#00a650',
+                '#00aa9c',
+                '#00adef',
+                '#0081cd',
+                '#005bab'
+            ];
+            let autoColor = false;
 
-    if (this.props.roundShape !== undefined) {
-      _roundShape = this.props.roundShape;
-      if (_roundShape) {
-        _defaultWrapperStyling.borderRadius = `${this.props.width}px`;
-        _img_styling.borderRadius = `${this.props.width}px`;
-      }
-    } else if (this.props.cornerRadius !== undefined) {
-      _corner_radius = this.props.cornerRadius;
-      _defaultWrapperStyling.borderRadius = `${_corner_radius}px`;
-      _img_styling.borderRadius = `${_corner_radius}px`;
+        // Create text to be shown
+
+        if (this.props.initials != undefined) {
+            str = this.props.initials;
+        }
+
+        if (this.props.string != undefined) {
+            str = this.getInitialsFromString(this.props.string);
+        }
+
+        // Calculate color
+
+        if (this.props.bgColor != undefined) {
+            bgColor = this.props.bgColor;
+        } else if (this.props.autoColor != undefined) {
+            autoColor = this.props.autoColor;
+            if (autoColor === true) {
+                let i;
+                    let lon = str.length;
+                    let charIndex = 0;
+                    let colorIndex;
+                for (i = 0; i < lon; i += 1) {charIndex = str.charCodeAt(i);}
+                colorIndex = charIndex % colorspalette.length;
+                bgColor = colorspalette[colorIndex];
+            }
+        }
+
+        // --------------------------
+        // Create the image here
+        // --------------------------
+
+        const imgData = this.generateAvatar(
+            str,
+            this.props.pictureResolution,
+            this.props.pictureResolution,
+            bgColor, // This.props.bgColor,
+            null,
+            this.props
+        );
+
+        // ------------------------------------------
+        // Create HTML and styles wraping the image
+        // ------------------------------------------
+
+        imgstyling = {
+            imageRendering: 'pixelated',
+            imageRendering: '-moz-crisp-edges'
+        };
+
+        pixelated = this.props.pixelated;
+        if (pixelated === true) {
+            imgstyling.imageRendering = 'pixelated';
+            // imgstyling.imageRendering = "-moz-crisp-edges";
+        }
+
+        defaultWrapperStyling = {
+            overflow: 'hidden',
+            width: `${this.props.width}px`,
+            height: `${this.props.width}px`,
+            boxSizing: 'unset'
+        };
+
+        if (this.props.roundShape !== undefined) {
+            roundShape = this.props.roundShape;
+            if (roundShape) {
+                defaultWrapperStyling.borderRadius = `${this.props.width}px`;
+                imgstyling.borderRadius = `${this.props.width}px`;
+            }
+        } else if (this.props.cornerRadius !== undefined) {
+            cornerradius = this.props.cornerRadius;
+            defaultWrapperStyling.borderRadius = `${cornerradius}px`;
+            imgstyling.borderRadius = `${cornerradius}px`;
+        }
+
+        defaultWrapperStyling = {
+            this.props.wrapperStyle,
+            defaultWrapperStyling
+        };
+
+        const defaultImageStyling = {
+            this.props.imgStyle,
+            imgstyling
+        };
+
+        const imgHtml = () => (
+            <img
+                src={imgData}
+                style={defaultImageStyling}
+                width={this.props.width}
+                height=""
+                alt="react-string-avatar"
+            />
+        );
+
+        if (this.props.wrapper) {
+            return (
+                <div className="avatar-wrapper " style={defaultWrapperStyling}>
+                    {' '}
+                    {imgHtml()}{' '}
+                </div>
+            );
+        }
+
+        return imgHtml();
     }
 
-    _defaultWrapperStyling = Object.assign(
-      {},
-      this.props.wrapperStyle,
-      _defaultWrapperStyling
-    );
+    componentDidMount () {
 
-    const _defaultImageStyling = Object.assign(
-      {},
-      this.props.imgStyle,
-      _img_styling
-    );
-
-    const imgHtml = () => (
-      <img
-        src={imgData}
-        style={_defaultImageStyling}
-        width={this.props.width}
-        height=""
-        alt="react-string-avatar"
-      />
-    );
-
-    if (this.props.wrapper) {
-      return (
-        <div className="avatar-wrapper " style={_defaultWrapperStyling}>
-          {imgHtml()}
-        </div>
-      );
     }
 
-    return imgHtml();
-  } // _renderImage ends here.
+    componentDidUpdate () {
 
-  render() {
-    return this._renderImage();
-  }
+    }
+
+    render = () => this.renderImage();
 }
 
 export default StringAvatar;
