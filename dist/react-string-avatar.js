c7abe9904b4ad0a05e2f0e3035a70ecc4394ec28
asr <ajsoriar@gmail.com>
2020-01-08 23:23:51 +0100

    v1.2.2

--- dist/react-string-avatar.js
+++ dist/react-string-avatar.js
@@ -188,7 +188,7 @@ exports.default = Avatar;
 /**
  * react-string-avatar
  * React String Avatar is a simple React component that generates a letter's avatar like Microsoft or Google do in their web apps. First letter of each word in a string or a group of initials will be used to generate the avatar. The image of the avatar will be rendered in an html img tag as a real png or jpeg. The image data can be retrieved using javascript to be stored in back-end giving you an initial profile picture in your web or mobile apps when the user does not upload one. Several React props are available to configure the output: size, shape, resolution, colors, etc.
- * @version v1.2.1 - 2020-01-08
+ * @version v1.2.2 - 2020-01-08
  * @link https://github.com/ajsoriar/react-string-avatar
  * @author Andres J. Soria R. <ajsoriar@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
